c8cda394ec75b04d34aaf7f6e0cf02b804f89516
Fahmid <fahmid.fasade@gmail.com>
2021-09-19 17:23:34 +1000

    Updating Appbar, Posts and Form components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./App.css";
+import { Container, Typography, Grow, Grid, AppBar } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Stories
+        </Typography>
+        <img src="./logo.png" alt="" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
