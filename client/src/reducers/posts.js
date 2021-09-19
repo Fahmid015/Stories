45b9309b42aee90bb03a33bbf7d97658c197ca70
Fahmid <fahmid.fasade@gmail.com>
2021-09-19 19:55:54 +1000

    Built Form UI and createpost action

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,9 @@
 export default (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
-      return posts;
+      return action.payload;
 
-    case "FETCH_ALL":
+    case "CREATE":
       return posts;
 
     default:
