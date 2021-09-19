2ceaa1526fba75039d2dd34d5cd19126063d9dcb
Fahmid <fahmid.fasade@gmail.com>
2021-09-19 18:35:23 +1000

    added api, styles to form, post and posts

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import { Container, Typography, Grow, Grid, AppBar } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Container>
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Stories
         </Typography>
-        <img src="./logo.png" alt="" height="60" />
+        <img src="./logo.png" className={classes.image} alt="" height="60" />
       </AppBar>
       <Grow in>
         <Container>
