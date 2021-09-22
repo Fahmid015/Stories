df3b8fc25da156d3fd1765a666f991c196a6e1c3
Fahmid <fahmid.fasade@gmail.com>
2021-09-22 19:19:33 +1000

    Added Home, Auth and Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { Container, Typography, Grow, Grid, AppBar } from "@material-ui/core";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+import { Container } from "@material-ui/core";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 function App() {
-  const [currentId, setCurrentId] = useState(null);
-
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
   return (
-    <Container>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Stories
-        </Typography>
-        <img src="./logo.png" className={classes.image} alt="" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            className={classes.mainContainer}
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <Router>
+      <Container>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/auth" exact>
+            <Auth />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
