64257abfe8edd7bf5350e00c74dba683093b3e3e
Fahmid Shabab Khorsheed <shabab015@gmail.com>
2022-05-10 21:38:36 +1000

    refactor

--- server/index.js
+++ server/index.js
@@ -26,10 +26,13 @@ const PORT = process.env.PORT || 5000;
 const mode = process.env.CONNECTION_URL;
 
 mongoose
-  .connect(mode, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://fahmid:Black01507@cluster0.7fxz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
